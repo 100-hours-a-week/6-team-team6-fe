d83a752bafd139fa3d7c8fcf551f7b0a294cf7f1
kkan9ma <kyeongmankang@gmail.com>
2026-01-26 17:12:14 +0900

    API 요청/응답/에러 관련 타입 정의

---
+++ shared/lib/api-error.d.ts
@@ -0,0 +1,24 @@
+import type { ApiErrorCode } from "@/shared/lib/error-codes";
+
+declare global {
+	namespace Api {
+		namespace Error {
+			type Code = ApiErrorCode;
+
+			interface Response {
+				errorCode: Code;
+			}
+
+			interface Login extends Response {
+				errorCode: Code.LOGIN_FAILED;
+			}
+
+			interface Signup extends Response {
+				// TODO: fix typo - EXSTING_ID
+				errorCode: Code.INVALID_LOGIN_ID_INPUT | Code.INVALID_PASSWORD_INPUT | Code.EXSTING_ID;
+			}
+		}
+	}
+}
+
+export {};
