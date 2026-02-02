40f975b458fd44db47795ed33f8de60fa101e120
kkan9ma <kyeongmankang@gmail.com>
2026-02-02 17:46:13 +0900

    AI 글 작성 기능 구현

--- features/post/components/PostItem.tsx
+++ features/post/components/PostItem.tsx
@@ -14,10 +14,6 @@ function PostItem(props: PostItemProps) {
 	const firstImageUrl = postFirstImageUrl || "/dummy-post-image.png";
 	const rentalFeeLabel = formatRentalFeeLabel(rentalFee, feeUnit);
 
-	console.log(`firstImageUrl`);
-	console.log(firstImageUrl);
-	console.log(`process.env.NEXT_PUBLIC_IMAGE_HOSTNAME`);
-	console.log(process.env.NEXT_PUBLIC_IMAGE_HOSTNAME);
 	return (
 		<div className="flex gap-x-4">
 			<div>
