db468179f42c92c3bf30e5fcc3063c813de1b6e6
kkan9ma <kyeongmankang@gmail.com>
2026-02-11 13:19:23 +0900

    물품 상세 페이지 캐러셀 수정

--- features/post/components/PostDetailImages.tsx
+++ features/post/components/PostDetailImages.tsx
@@ -13,26 +13,42 @@ type PostDetailImagesProps = {
 function PostDetailImages(props: PostDetailImagesProps) {
 	const { images } = props;
 
+	if (images.length === 0) {
+		return null;
+	}
+
+	if (images.length === 1) {
+		return (
+			<div className="relative w-full aspect-square">
+				<Image
+					fill
+					src={images[0].imageUrl}
+					alt="Post image"
+					sizes="100vw"
+					className="object-cover"
+				/>
+			</div>
+		);
+	}
+
 	return (
-		<div>
-			<Carousel showDots>
-				<CarouselContent>
-					{images.map((image, index) => (
-						<CarouselItem key={image.postImageId} className="pl-0">
-							<div className="relative w-full aspect-square">
-								<Image
-									fill
-									src={image.imageUrl}
-									alt={`Post image ${index + 1}`}
-									sizes="100vw"
-									className="object-cover"
-								/>
-							</div>
-						</CarouselItem>
-					))}
-				</CarouselContent>
-			</Carousel>
-		</div>
+		<Carousel showDots>
+			<CarouselContent>
+				{images.map((image) => (
+					<CarouselItem key={image.postImageId} className="pl-0">
+						<div className="relative w-full aspect-square">
+							<Image
+								fill
+								src={image.imageUrl}
+								alt={`Post image`}
+								sizes="100vw"
+								className="object-cover"
+							/>
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+		</Carousel>
 	);
 }
 
