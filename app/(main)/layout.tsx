5cd2350ceedfb901e485b4f3df1d2cc200c96a74
kkan9ma <kyeongmankang@gmail.com>
2026-02-24 16:21:24 +0900

    Webpush 수신 점검 위한 테스트 코드 추가

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 
 import { ChatInboxRealtimeBridge } from "@/features/chat/components/ChatInboxRealtimeBridge";
+import { WebPushForegroundBridge } from "@/features/notification/components/WebPushForegroundBridge";
 
 import { authOptions } from "@/shared/lib/auth";
 
@@ -22,6 +23,7 @@ export default async function MainLayout({ children }: MainLayoutProps) {
 	return (
 		<AuthSessionProvider session={session} unauthenticatedRedirect="/login">
 			<ChatInboxRealtimeBridge />
+			<WebPushForegroundBridge />
 			{children}
 		</AuthSessionProvider>
 	);
